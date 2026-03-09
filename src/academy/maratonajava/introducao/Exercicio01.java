dd3c6090c6f709c37127ffc039e1e49e5c922c03
Matheus Henrique <matheus.braz@transformatechadvisors.com>
2026-03-09 14:26:49 -0300

    exercicio 01 finalizado

--- src/academy/maratonajava/introducao/Exercicio01.java
+++ src/academy/maratonajava/introducao/Exercicio01.java
@@ -1,5 +1,22 @@
 package academy.maratonajava.introducao;
 
-public class Exercicio01 {
+/*
+PRATICA:
 
+Crie variaveis para os campos descritos abaixo e imprima entre <> e imprima a seguinte mensagem:
+
+Eu <nome> morando no endereço <endereço>
+confirmo que recebi o salario de <salario>, na data <data>
+ */
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        String nome = "Batman";
+        String endereco = "GotanCity";
+        double salario = 2000.0;
+        String data = "02/06/2026";
+
+        System.out.println("Eu "+nome+ " morando no endereço " +endereco+
+                " confirmo que recebi o salario de " +salario+ ", na data "+data);
+    }
 }
