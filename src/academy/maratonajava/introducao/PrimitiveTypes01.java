6ed1e49361f61e45609cd81e115a3fbd6ead1960
Matheus Henrique <matheus.braz@transformatechadvisors.com>
2026-03-09 13:15:15 -0300

    first commit

---
+++ src/academy/maratonajava/introducao/PrimitiveTypes01.java
@@ -0,0 +1,21 @@
+package academy.maratonajava.introducao;
+
+public class PrimitiveTypes01 {
+    public static void main(String[] args) {
+        // int, double, float, chat, byte, short, long, boolean
+        int idade = 21; // numeros inteiros 4 bytes
+        long numeroGrande = 100000;
+        double salarioDouble = 2000; // numeros com virgula 8 bytes
+        float salarioFloat = 2500; // numeros com virgula 4 bytes
+        byte idadeByte = -128; // maximo 127 de tamanho 1 byte
+        short idadeShort = 10;
+        boolean verdadeiro = true; // velores logicos (verdadeiro ou falso) um unico bit (0/1)
+        boolean falso = false; // velores logicos (verdadeiro ou falso) um unico bit (0/1)
+        char caractere = 65; // traduz um valor numerico para tabela ASCII ou unicode!
+
+        System.out.println("A idade é: "+idade+" anos.");
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+        System.out.println("char: "+caractere);
+    }
+}
