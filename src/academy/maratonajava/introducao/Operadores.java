90c580a1e7bb6520a2706942ced3b7acf683d321
Matheus Henrique <matheus.braz@transformatechadvisors.com>
2026-03-10 19:58:08 -0300

    adicionando parte de operadores de atribuição

--- src/academy/maratonajava/introducao/Operadores.java
+++ src/academy/maratonajava/introducao/Operadores.java
@@ -17,5 +17,79 @@ public class Operadores {
 
         double divisao = (double) numero01 / numero02;
         System.out.println(divisao);
+
+        //-------------------------------------------------------
+
+        // %, <, >, <=, >=, ==, != <- operadores relacionais
+
+        // %
+        int resto = 21 % 2;
+        System.out.println(resto);
+
+        // <, >, <=, >=, ==, !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualAVinte = 10 == 20;
+        boolean isDezIgualADez = 10 == 10;
+        boolean isDezDiferenteDeDez = 10 != 10;
+
+        System.out.println(isDezMaiorQueVinte);
+        System.out.println(isDezMenorQueVinte);
+        System.out.println(isDezIgualAVinte);
+        System.out.println(isDezIgualADez);
+        System.out.println(isDezDiferenteDeDez);
+
+        //-------------------------------------------------------
+
+        // &&(AND), ||(OR), !(NOT) <- operadores lógicos
+        int idade = 29;
+        float salario = 3500F;
+
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println(isDentroDaLeiMaiorQueTrinta);
+        System.out.println(isDentroDaLeiMenorQueTrinta);
+
+        //-------------------------------------------------------
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println(isPlaystationCincoCompravel);
+
+        //-------------------------------------------------------
+
+        // =, +=, -=, *=, /=, %= <- operadores de atribuição
+
+        double bonus = 1800;
+        bonus += 1000; // bonus = bonus + 1000;
+        System.out.println(bonus);
+        bonus -= 1000; // bonus = bonus - 1000;
+        System.out.println(bonus);
+        bonus *= 2; // bonus = bonus * 1000;
+        System.out.println(bonus);
+        bonus /= 2; // bonus = bonus / 1000;
+        System.out.println(bonus);
+        bonus %= 2; // bonus = bonus % 1000;
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador += 1; // contador = contador + 1;
+        contador++; // incremento de 1 // ++contador igual à contador++
+        contador--; // decremento de 1 // --contador igual à contador--
+        System.out.println(contador);
+
+        /*
+        Comportamento da diferença entre pós-incremento
+        (contador2++) e pré-incremento (++contador2) em Java:
+        */
+        int contador2 = 0;
+        System.out.println(contador2++);
+        System.out.println(contador2);
+        System.out.println(++contador2);
     }
 }
